7351b0ebeaaf7956ec84dc6a7c09595d1baf7d34
amosbastian <amosbastian@gmail.com>
2019-10-06 15:22:02 +0100

    Updates

---
+++ precache-manifest.3074839f5a6604b1e6ebb8fbbdcacd53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac22e78ea3ce8a96960eaf763d7893b6",
+    "url": "/unbabel-translate/index.html"
+  },
+  {
+    "revision": "bb270e76ed715a5a4ceb",
+    "url": "/unbabel-translate/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "f21aceb225762d596161",
+    "url": "/unbabel-translate/static/css/main.1264657e.chunk.css"
+  },
+  {
+    "revision": "bb270e76ed715a5a4ceb",
+    "url": "/unbabel-translate/static/js/2.b47265ef.chunk.js"
+  },
+  {
+    "revision": "f21aceb225762d596161",
+    "url": "/unbabel-translate/static/js/main.b221ad59.chunk.js"
+  },
+  {
+    "revision": "470b596291fd2ce3711c",
+    "url": "/unbabel-translate/static/js/runtime-main.3acc9986.js"
+  },
+  {
+    "revision": "6a891f45bd955974a17927c59488cb11",
+    "url": "/unbabel-translate/static/media/unbabel-logo.6a891f45.svg"
+  }
+]);
